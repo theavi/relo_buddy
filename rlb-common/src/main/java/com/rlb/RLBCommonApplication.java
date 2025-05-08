740472ddada78d4b8c5b197a4a723e95763cc332
Avinash <khadsanavinash01@gmail.com>
2025-05-08 23:05:19 +0530

    RLB-master whats app integration

---
+++ rlb-common/src/main/java/com/rlb/RLBCommonApplication.java
@@ -0,0 +1,11 @@
+package com.rlb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RLBCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RLBCommonApplication.class,args);
+    }
+}
