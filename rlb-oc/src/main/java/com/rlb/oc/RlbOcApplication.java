54de6522d50dfd57004834b7c446e2e7f8ab2975
Aniket-Sable <aniketsablepatil99k@gmail.com>
2025-05-25 15:17:43 +0530

    RLB-00 temp

--- rlb-oc/src/main/java/com/rlb/oc/RlbOcApplication.java
+++ rlb-oc/src/main/java/com/rlb/oc/RlbOcApplication.java
@@ -1,14 +1,18 @@
 package com.rlb.oc;
 
+import com.rlb.oc.model.Role;
+import com.rlb.oc.model.User;
+import com.rlb.oc.repository.RoleRepository;
+import com.rlb.oc.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 //@Import(SharedConfig.class)
 public class RlbOcApplication {
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(RlbOcApplication.class, args);
 
