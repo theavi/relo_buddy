353d38b7c2e6d45b67054aaa0045cca18145e0fa
Aniket-Sable <aniketsablepatil99k@gmail.com>
2025-06-01 18:16:11 +0530

    RLB-04 Added custom ErrorResponse in RLB-OH

---
+++ rlb-oh/src/main/java/com/rlb/oh/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.rlb.oh.exception;
+
+import com.rlb.oc.exception.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = {RecordNotFound.class})
+    public ResponseEntity<ErrorResponse> recordNotFoundExceptionHandler(RecordNotFound ex, HttpServletRequest request){
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(),
+                                                        HttpStatus.NOT_FOUND.getReasonPhrase(),
+                                                        ex.getMessage(),
+                                                        request.getRequestURI());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<ErrorResponse> defaultExceptionHandler(Exception ex, HttpServletRequest request){
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
