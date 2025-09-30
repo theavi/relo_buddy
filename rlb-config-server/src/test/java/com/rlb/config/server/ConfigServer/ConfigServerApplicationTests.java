dba4eac1ae712f7af89fd8d1dc046ee43ccd71c5
Aniket-Sable <aniketsablepatil99k@gmail.com>
2025-09-30 18:08:21 +0530

    RLB- Master Added Spring Cloud Config Server

---
+++ rlb-config-server/src/test/java/com/rlb/config/server/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlb.config.server.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
