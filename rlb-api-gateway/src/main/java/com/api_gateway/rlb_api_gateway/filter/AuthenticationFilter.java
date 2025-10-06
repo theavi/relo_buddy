6f7b022ac183c6ac965981f979291d9a3a7a373a
Aniket-Sable <aniketsp08@gmail.com>
2025-10-06 21:04:26 +0530

    RLB-08 added filter in rlb-gateway service

--- rlb-api-gateway/src/main/java/com/api_gateway/rlb_api_gateway/filter/AuthenticationFilter.java
+++ rlb-api-gateway/src/main/java/com/api_gateway/rlb_api_gateway/filter/AuthenticationFilter.java
@@ -26,6 +26,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
         return ((exchange, chain) -> {
             if (validator.isSecured.test(exchange.getRequest())) {
                 //header contains token or not
+
                 if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
                     throw new RuntimeException("missing authorization header");
                 }
--- rlb-auth/src/main/java/com/rlb/auth/AuthServiceApplication.java
+++ rlb-auth/src/main/java/com/rlb/auth/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package com.rlb.auth.service;
+package com.rlb.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
